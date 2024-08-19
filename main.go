62c70195f30d871d34824e50773a8ec1d5814c0c
hendisantika <hendisantika@yahoo.co.id>
2024-08-19 18:02:43 +0700

    Add log info🫸🌀✏️📗🐧🐳⬆

--- main.go
+++ main.go
@@ -31,10 +31,11 @@ func sendEmail(message string, toAddress string) (response bool, err error) {
 	fromAddress := os.Getenv("EMAIL")
 	fromEmailPassword := os.Getenv("PASSWORD")
 	smtpServer := os.Getenv("SMTP_SERVER")
-	smptPort := os.Getenv("SMTP_PORT")
+	smtpPort := os.Getenv("SMTP_PORT")
 
 	var auth = smtp.PlainAuth("", fromAddress, fromEmailPassword, smtpServer)
-	err = smtp.SendMail(smtpServer+":"+smptPort, auth, fromAddress, []string{toAddress}, []byte(message))
+	err = smtp.SendMail(smtpServer+":"+smtpPort, auth, fromAddress, []string{toAddress}, []byte(message))
+	log.Println("EMail sent successfully!")
 	if err == nil {
 		return true, nil
 	}
@@ -51,6 +52,7 @@ func consume(ctx context.Context) {
 
 	for {
 		msg, err := r.ReadMessage(ctx)
+		log.Println("Consume message")
 		if err != nil {
 			panic("could not read message " + err.Error())
 		}
@@ -72,18 +74,5 @@ func consume(ctx context.Context) {
 }
 
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
-	}
+	consume(context.Background())
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
