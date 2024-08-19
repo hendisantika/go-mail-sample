8ab7ccef3998c841afbf85dd62654f468a285954
hendisantika <hendisantika@yahoo.co.id>
2024-08-19 17:56:45 +0700

    Add init .env file🫸🌀✏️📗🐧🐳⬆

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"time"
 )
 
@@ -10,6 +12,13 @@ type User struct {
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
 
 func main() {
